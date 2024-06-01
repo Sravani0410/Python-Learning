0ee065351c9ae2dc8a9bf800edc7af2f8117101f
sravani <sandhyapyla.8@gmail.com>
2024-06-01 15:40:27 +0530

    learning "comment" concept

---
+++ README.md
@@ -0,0 +1,3 @@
+## comments
+ 1. single line comment(#)
+ 2. multi line comment('''  write the comments here ''')
---
+++ comment.py
@@ -0,0 +1,7 @@
+'''
+1. single line
+2. multi line
+'''
+# below statement will print something
+
+print("hello")
